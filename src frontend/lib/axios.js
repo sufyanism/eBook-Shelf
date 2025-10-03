2f96d30dde0275557ef79f333e26789c89ca5cf1
Sufyan bin Uzayr <42644879+sufyanism@users.noreply.github.com>
2025-10-03 12:09:24 +0530

    Delete src frontend directory

--- src frontend/lib/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-export const axiosInstance = new axios.create({
-    baseURL:import.meta.env.MODE === "development"?"http://localhost:5000":"https://book-shelf-backend-dhuo.onrender.com",
-    withCredentials:true,
-})
